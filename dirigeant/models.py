1f51902e43d344f88b7dc9c897c809aacf279a69
hermann <hermann.kass@gmail.com>
2021-11-03 12:05:49 +0000

    mise en relation de commune et leader

--- dirigeant/models.py
+++ dirigeant/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from composition.models import Commune
 
 # Create your models here.
 class Leader(models.Model):
+    commune = models.ForeignKey(Commune, related_name="leaders", on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=_("name"))
     fonction = models.CharField(max_length=100, verbose_name=_("fonction"))
     description = models.TextField(_("description"), blank=True, null=True)
