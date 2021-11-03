fd6b6526e4af4f624569cd0ce4906a0da3d2ecc9
hermann <hermann.kass@gmail.com>
2021-11-03 12:09:26 +0000

    ajout des classes dans admin.py

--- composition/admin.py
+++ composition/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Commune,Prefecture,Canton,Quartier,Region,Village
 # Register your models here.
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Prefecture)
+class PrefectureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Commune)
+class CommuneAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Canton)
+class CantonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Village)
+class VillageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Quartier)
+class QuartierAdmin(admin.ModelAdmin):
+    pass
