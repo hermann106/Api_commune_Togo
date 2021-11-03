69c83f5ed1d666166a7f25d3c68ae36eca9014fa
hermann <hermann.kass@gmail.com>
2021-11-03 11:58:07 +0000

    maj du projet

--- composition/models.py
+++ composition/models.py
@@ -2,12 +2,79 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class Composition(models.Model):
+class Region(models.Model):
     name = models.CharField(max_length=100, verbose_name=_("name"))
-    agglomeration = models.CharField(max_length=100, verbose_name=_("name"), help_text=_("Canton or Quartier"))
     description = models.TextField(_("description"), blank=True, null=True)
     coordonate = models.JSONField(blank=True, null=True, verbose_name=_("Coordonate"))
-    prefecture = models.CharField(max_length=100, verbose_name=_("prefecture"))
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-created_at"]
+
+class Prefecture(models.Model):
+    region = models.ForeignKey(Region, related_name="prefectures", on_delete=models.CASCADE )
+    name = models.CharField(max_length=100, verbose_name=_("name"))
+    description = models.TextField(_("description"), blank=True, null=True)
+    coordonate = models.JSONField(blank=True, null=True, verbose_name=_("Coordonate"))
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-created_at"]
+
+class Commune(models.Model):
+    prefecture = models.ForeignKey(Prefecture, related_name="communes", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=_("name"))
+    description = models.TextField(_("description"), blank=True, null=True)
+    coordonate = models.JSONField(blank=True, null=True, verbose_name=_("Coordonate"))
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-created_at"]
+
+class Canton(models.Model):
+    commune = models.ForeignKey(Commune, related_name="cantons", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=_("name"))
+    description = models.TextField(_("description"), blank=True, null=True)
+    coordonate = models.JSONField(blank=True, null=True, verbose_name=_("Coordonate"))
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-created_at"]
+
+class Village(models.Model):
+    canton = models.ForeignKey(Canton, related_name="villages", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=_("name"))
+    description = models.TextField(_("description"), blank=True, null=True)
+    coordonate = models.JSONField(blank=True, null=True, verbose_name=_("Coordonate"))
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-created_at"]
+
+class Quartier(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("name"))
+    description = models.TextField(_("description"), blank=True, null=True)
+    coordonate = models.JSONField(blank=True, null=True, verbose_name=_("Coordonate"))
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
