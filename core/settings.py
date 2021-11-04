5b37845bdc415ebade23e2f634ff7ced8220d3f4
hermann <hermann.kass@gmail.com>
2021-11-04 15:31:24 +0000

    settings.py

--- core/settings.py
+++ core/settings.py
@@ -122,4 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
